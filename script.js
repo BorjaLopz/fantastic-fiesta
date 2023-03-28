3b22a0e617e2582b4c14efe8c2549bae40bfd064
Borja <borja.lpzdz5@gmail.com>
2023-03-28 11:49:07 +0200

    Obtenemos información de la ubicación consultando previamente permisos de ubicación del navegador

--- script.js
+++ script.js
@@ -32,47 +32,60 @@ const darkModeButton = document.querySelector("#darkModeButton");
 */
 
 /*  Variables y constantes para usar con la API */
-const APIKey = "2238b138004bfdcffd5a7e524cab218e";  //Licencia de la API que usaremos cuando la llamemos
-const lang = "es";  //Sacaremos los datos en español. Solamente se aplicará en el nombre de la ciudad y la descripción. 
-const units = "metric"; //Usaremos los datos en sistema métrico. 
-
+const APIKey = "2238b138004bfdcffd5a7e524cab218e"; //Licencia de la API que usaremos cuando la llamemos
+const lang = "es"; //Sacaremos los datos en español. Solamente se aplicará en el nombre de la ciudad y la descripción.
+const units = "metric"; //Usaremos los datos en sistema métrico.
 
 /* Declaramos variables globales para poder usar donde queramos latitud y longitud. */
 let latitude;
 let longitude;
 
-
 /* Función para conmutar modo oscuro */
-function handleDarkModeButton()
-{
-    //Nombre de la clase provisional.
-    html.classList.toggle(".darkModeButton");
-    console.log("DarkMode:", html.classList.contains(".darkModeButton"))
+function handleDarkModeButton() {
+  //Nombre de la clase provisional.
+  html.classList.toggle(".darkModeButton");
+  console.log("DarkMode:", html.classList.contains(".darkModeButton"));
 }
 
 //Event listener onClick del boton darkMode
 darkModeButton.addEventListener("click", handleDarkModeButton);
 
-
 //Obtenemos coordinadas (latitud y longitud)
-function getCoordinates()
-{
-    if("geolocation" in navigator)
-    {
-        //Funciona la geolocalizacion en el navegador
-        navigator.geolocation.getCurrentPosition((position) => 
-        {
-            latitude = position.coords.latitude;
-            longitude = position.coords.longitude;
-        });
+function getPermissionOfLocation() {
+  if ("geolocation" in navigator) {
+    //Funciona la geolocalizacion en el navegador
 
-        console.log(`Latitud: ${latitude}\nLongitud: ${longitude}`);
-    }
-    else
+    //Permisos de ubicacion, asi podremos ver el state en el que se encuentra actualmente
+    // navigator.permissions.query({ name: "geolocation" }).then(console.log);
+    navigator.permissions.query({ name: "geolocation" }).then((result) =>
     {
-        //No funciona la geolocalización en el navegador
-    }
+      //Comprobamos el estado actual, salvo que este en denied haremos la llamada para obtener la ubicación. 
+      if(result.state !== "denied")
+      {
+        statusLocation.textContent = `Localizando ...`;
+        navigator.geolocation.getCurrentPosition((position) => {
+          latitude = position.coords.latitude.toFixed(2); //Obtenemos solamente 2 decimales usando .toFixed(2) -> https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed
+          longitude = position.coords.longitude.toFixed(2);
+
+          logCoordinates();
+        });
+      }
+      else
+      {
+        statusLocation.textContent = "No se pudo localizar. Activa antes la ubicación de tu dispositivo. ";
+      }
+    });
+    
+
+  } else {
+    //No funciona la geolocalización en el navegador
+  }
 }
 
-//Probamos funcion obtener coordenadas. 
-locationButton.addEventListener("click", getCoordinates);
+//Mostramos en un parrafo las coordenadas
+function logCoordinates() {
+  statusLocation.textContent = `Latitud: ${latitude} Longitud: ${longitude}`;
+}
+
+//Probamos funcion obtener coordenadas.
+locationButton.addEventListener("click", getPermissionOfLocation);
