1003e82658183beaad81113bd4723e940904c6c9
HACKER <you@example.com>
2023-03-29 17:19:07 +0200

    cambio desconocido

--- script.js
+++ script.js
@@ -147,6 +147,12 @@ async function getIconFromAPI(arr) {
   console.log(arr.weather);
   for (const it of arr) {
     let [{ icon }] = it.weather;
+    let url = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+  }
+}
+async function getIconFromAPI(arr) {
+  for (const it of arr) {
+    let [{ icon }] = it.weather;
     let url = `https://openweathermap.org/img/wn/${icon}@4x.png`; //Esto lo tendremos que meter en la etiqueta img dentro del src
 
     console.log(url);
